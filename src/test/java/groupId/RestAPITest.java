ffcbf8318c407c86b7771502195cdb1bc711b318
Gomatigirhepuje <gomagirhepuje@gmail.com>
2019-05-21 19:24:10 +0530

    first step

---
+++ src/test/java/groupId/RestAPITest.java
@@ -0,0 +1,16 @@
+package groupId;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void postJira(){
+	System.out.println("postJira");	
+	}
+	
+	@Test
+	public void elementsUI(){
+	System.out.println("elementsUI");
+	}
+
+}
